48803b386a413a0d87e0c3a5bb690ed2912d7244
LathaSriramulu <srilatha0726@gmail.com>
2017-02-11 16:28:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article1', function (req, res) {
+app.get('/article1.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 
-app.get('/article2', function (req, res) {
+app.get('/article2.html', function (req, res) {
     res.send('article2 requested and will be served here');
 });
 
-app.get('/article3', function (req, res) {
+app.get('/article3.html', function (req, res) {
     res.send('article3 requested and will be served here');
 });
 
